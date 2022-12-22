402ccdc22fb8ed3ed6bfb2d3a88a332f34359f25
Abdullah Younus <abdullah1001k@gmail.com>
2022-12-22 16:18:58 +0500

    done

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 import inquirer from 'inquirer';
-const answers = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'person_id',
-        message: 'Eneter Your Id :'
-    },
-    {
-        type: 'number',
-        name: 'person_pin',
-        message: 'Enter Your Pin :'
-    },
-    {
-        type: 'list',
-        name: 'cardType',
-        choices: ['Current', 'Saving'],
-        message: 'Select Your Card Type :'
-    },
-    {
-        type: 'list',
-        name: 'transaction',
-        choices: ['Fast Cash', 'Withdraw'],
-        message: 'Select Your Transaction',
-        when(answers) {
-            return answers.cardType;
+import chalAnimation from 'chalk-animation';
+const sleep = (time = 2000) => new Promise((resovle, reject) => setTimeout(resovle, time));
+async function welcome() {
+    const rainbowTitle = chalAnimation.rainbow('ATM Project');
+    await sleep();
+    rainbowTitle.stop();
+}
+// welcome();
+async function atm() {
+    await welcome();
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'person_id',
+            message: 'Eneter Your Id :'
+        },
+        {
+            type: 'number',
+            name: 'person_pin',
+            message: 'Enter Your Pin :'
+        },
+        {
+            type: 'list',
+            name: 'cardType',
+            choices: ['Current', 'Saving'],
+            message: 'Select Your Card Type :'
+        },
+        {
+            type: 'list',
+            name: 'transaction',
+            choices: ['Fast Cash', 'Withdraw'],
+            message: 'Select Your Transaction',
+            when(answers) {
+                return answers.cardType;
+            }
+        },
+        {
+            type: 'list',
+            name: 'amount',
+            choices: [1000, 2000, 5000],
+            message: 'Select Your Amount :',
+            when(answers) {
+                return answers.transaction === 'Fast Cash';
+            }
+        },
+        {
+            type: 'number',
+            name: 'amount',
+            message: 'Enter Your Amount',
+            when(answers) {
+                return answers.transaction === 'Withdraw';
+            }
         }
-    },
-    {
-        type: 'list',
-        name: 'paymentMethod',
-        choices: [1000, 2000, 5000],
-        message: 'Select Your Amount :',
-        when(answers) {
-            return answers.transaction === 'Fast Cash';
+    ]);
+    if (answers.person_id && answers.person_pin) {
+        const balance = Math.floor(Math.random() * 10000000);
+        console.log("balance ====>", balance);
+        const enterAmount = answers.amount;
+        if (balance >= enterAmount) {
+            const currentAmount = balance - enterAmount;
+            console.log('Current Amount ==>', currentAmount);
         }
-    },
-    {
-        type: 'number',
-        name: 'amount',
-        message: 'Enter Your Amount',
-        when(answers) {
-            return answers.transaction === 'Withdraw';
+        else {
+            console.log('Insufficient Balance');
         }
     }
-]);
-console.log('Answers :', answers);
+}
+atm();
--- index.ts
+++ index.ts
@@ -1,55 +1,86 @@
 import inquirer from 'inquirer';
+import chalk from 'chalk';
+import chalAnimation from 'chalk-animation';
 
+const sleep = (time = 2000) => new Promise((resovle, reject) => setTimeout(resovle, time));
 
-interface UserType {
-    person_id: string,
-    person_pin: number,
+async function welcome() {
+    const rainbowTitle = chalAnimation.rainbow('ATM Project');
+    await sleep();
+    rainbowTitle.stop();
 }
+// welcome();
 
-const answers: UserType = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'person_id',
-        message: 'Eneter Your Id :'
-    },
-    {
-        type: 'number',
-        name: 'person_pin',
-        message: 'Enter Your Pin :'
-    },
-    {
-        type: 'list',
-        name: 'cardType',
-        choices: ['Current', 'Saving'],
-        message: 'Select Your Card Type :'
-    },
-    {
-        type: 'list',
-        name: 'transaction',
-        choices: ['Fast Cash', 'Withdraw'],
-        message: 'Select Your Transaction',
-        when(answers) {
-            return answers.cardType
+async function atm() {
+    await welcome();
+    interface UserType {
+        person_id: string,
+        person_pin: number,
+        cardType: string,
+        transaction: string,
+        amount: number
+    }
+    const answers: UserType = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'person_id',
+            message: 'Eneter Your Id :'
+        },
+        {
+            type: 'number',
+            name: 'person_pin',
+            message: 'Enter Your Pin :'
+        },
+        {
+            type: 'list',
+            name: 'cardType',
+            choices: ['Current', 'Saving'],
+            message: 'Select Your Card Type :'
+        },
+        {
+            type: 'list',
+            name: 'transaction',
+            choices: ['Fast Cash', 'Withdraw'],
+            message: 'Select Your Transaction',
+            when(answers) {
+                return answers.cardType
+            }
+        },
+        {
+            type: 'list',
+            name: 'amount',
+            choices: [1000, 2000, 5000],
+            message: 'Select Your Amount :',
+            when(answers) {
+                return answers.transaction === 'Fast Cash'
+            }
+        },
+        {
+            type: 'number',
+            name: 'amount',
+            message: 'Enter Your Amount',
+            when(answers) {
+                return answers.transaction === 'Withdraw'
+            }
+
         }
-    },
-    {
-        type: 'list',
-        name: 'paymentMethod',
-        choices: [1000, 2000, 5000],
-        message: 'Select Your Amount :',
-        when(answers) {
-            return answers.transaction === 'Fast Cash'
+    ])
+    if (answers.person_id && answers.person_pin) {
+        const balance = Math.floor(Math.random() * 10000000);
+        console.log("balance ====>", balance);
+        const enterAmount = answers.amount;
+        if (balance >= enterAmount) {
+            const currentAmount = balance - enterAmount
+            console.log('Current Amount ==>', currentAmount);
         }
-    },
-    {
-        type: 'number',
-        name: 'amount',
-        message: 'Enter Your Amount',
-        when(answers) {
-            return answers.transaction === 'Withdraw'
+        else {
+            console.log('Insufficient Balance');
         }
 
     }
-])
 
-console.log('Answers :', answers);
+}
+
+
+
+atm();
